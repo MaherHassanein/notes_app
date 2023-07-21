6028fcd4b736f52b8938c62c428bbd537e48e012
Your Name <you@example.com>
2023-07-21 03:06:35 +0300

    add simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/add_note_cubit/cubit/addnote_cubit.dart';
 import 'package:notes_app/costant/costant.dart';
 import 'package:notes_app/modal/notes_modal.dart';
+import 'package:notes_app/simples/simple_bloc_observer.dart';
 import 'package:notes_app/views/notes_view.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Bloc.observer = SimpleBlocObserver();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NotesModalAdapter());
   runApp(NotesApp());
--- lib/widgets/add_note_buttomsheet.dart
+++ lib/widgets/add_note_buttomsheet.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_textbuttom.dart';
 import 'package:notes_app/widgets/custom_textfield.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/add_note_cubit/cubit/addnote_cubit.dart';
 
 class AddNoteButtom extends StatefulWidget {
   const AddNoteButtom({super.key});
@@ -13,6 +16,7 @@ class _AddNoteButtomState extends State<AddNoteButtom> {
   GlobalKey<FormState> formkey = GlobalKey();
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
   String? title, subtitle;
+  bool isloading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,38 +24,54 @@ class _AddNoteButtomState extends State<AddNoteButtom> {
       key: formkey,
       autovalidateMode: autovalidateMode,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            CustomTextField(
-              onsaved: (data) {
-                title = data;
-              },
-              hint: 'Title',
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            CustomTextField(
-              onsaved: (data) {
-                subtitle = data;
-              },
-              hint: 'Content',
-              maxlines: 6,
-            ),
-            SizedBox(
-              height: 90,
-            ),
-            CustomTextButtom(
-              ontap: () {
-                if (formkey.currentState!.validate()) {
-                  formkey.currentState!.save();
-                } else {
-                  autovalidateMode = AutovalidateMode.always;
-                  setState(() {});
-                }
-              },
-            ),
-          ],
+        child: BlocConsumer<AddnoteCubit, AddnoteState>(
+          listener: (context, state) {
+            if (state is AddnoteLoading) {
+              isloading = true;
+            } else if (state is AddnoteSuccess) {
+              Navigator.pop(context);
+            } else if (state is AddnoteFailure) {
+              print('failied  $state.errormassege');
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: isloading,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    onsaved: (data) {
+                      title = data;
+                    },
+                    hint: 'Title',
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  CustomTextField(
+                    onsaved: (data) {
+                      subtitle = data;
+                    },
+                    hint: 'Content',
+                    maxlines: 6,
+                  ),
+                  SizedBox(
+                    height: 90,
+                  ),
+                  CustomTextButtom(
+                    ontap: () {
+                      if (formkey.currentState!.validate()) {
+                        formkey.currentState!.save();
+                      } else {
+                        autovalidateMode = AutovalidateMode.always;
+                        setState(() {});
+                      }
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
